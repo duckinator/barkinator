60bd85fa47aa8ac17ee609c432492a48e8dd0f91
Ellen Marie Dash <me@duckie.co>
2017-12-17 01:06:37 -0500

    ?!?!?!?! IT SEEMS TO BE KIND OF WORKING?!

--- fuck.c
+++ fuck.c
@@ -3,66 +3,65 @@
 #include <stdbool.h>
 
 SDL_AudioSpec wanted;
-void fill_audio(void *udata, uint8_t *stream, int length);
+bool keep_running = true;
 
+void cleanup() {
+    keep_running = false;
+    SDL_CloseAudio();
+}
+
+/*
+ * Generate audio and shove it into the output stream.
+ *
+ * +udata+:  Fuck if I even know what this is for.
+ * +stream+: Pointer to the audio buffer to be filled.
+ * +length+: The length (in bytes) of the audio buffer.
+ */
+void fill_audio(void *udata, uint8_t *stream, int length)
+{
+    uint8_t *buffer = malloc(length);
+    memset(buffer, 0, length);
+
+    SDL_MixAudio(stream, buffer, length, SDL_MIX_MAXVOLUME / 2);
+
+    free(buffer);
+}
+
+// Acquire access to an audio device.
 bool open_audio_device()
 {
-    /* Set the audio format */
+    // Specify wanted audio format.
     wanted.freq = 22050;
     wanted.format = AUDIO_U16;
-    wanted.channels = 1;    /* 1 = mono, 2 = stereo */
-    wanted.samples = 1024;  /* Good low-latency value for callback */
+    wanted.channels = 1;    // 1 = mono, 2 = stereo.
+    wanted.samples = 1024;  // According to the internet: "Good low-latency value for callback."
     wanted.callback = fill_audio;
     wanted.userdata = NULL;
 
-    /* Open the audio device, forcing the desired format */
-    if ( SDL_OpenAudio(&wanted, NULL) < 0 ) {
-        fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
+    // Open the audio device, forcing the desired format.
+    if (SDL_OpenAudio(&wanted, NULL) < 0) {
+        fprintf(stderr, "Couldn't open audio device: %s\n", SDL_GetError());
         return false;
     }
 
     return true;
 }
 
-static uint8_t *audio_chunk;
-static int audio_length;
-static uint8_t *audio_position;
-
-/* The audio function callback takes the following parameters:
- * stream:  A pointer to the audio buffer to be filled
- * len:     The length (in bytes) of the audio buffer
- */
-void fill_audio(void *udata, uint8_t *stream, int length)
-{
-    /* Only play if we have data left */
-    if ( audio_length == 0 )
-        return;
-
-    /* Mix as much data as possible */
-    length = (audio_length < length) ? audio_length : length;
-    SDL_MixAudio(stream, audio_position, length, SDL_MIX_MAXVOLUME / 2);
-    audio_position += length;
-    audio_length   -= length;
-}
-
 int main(int argc, char *argv[])
 {
-    /* Load the audio data ... */
+    // Always run cleanup() before exiting.
+    atexit(cleanup);
 
-    ;;;;;
-
-    audio_position = audio_chunk;
+    if (!open_audio_device()) {
+        return 1;
+    }
 
     /* Let the callback function play the audio chunk */
     SDL_PauseAudio(0);
 
-    /* Do some processing */
-
-    ;;;;;
-
-    /* Wait for sound to complete */
-    while ( audio_length > 0 ) {
-        SDL_Delay(100);         /* Sleep 1/10 second */
+    while (keep_running) {
+        SDL_Delay(100); // Sleep 1/10th of a second.
     }
-    SDL_CloseAudio();
+
+    return 0;
 }
