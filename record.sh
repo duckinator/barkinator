2b027f5e638339f997bb7b2b586a215bbac2ab1f
Ellen Marie Dash <me@duckie.co>
2017-12-18 03:32:54 -0500

    don't background processes in record.sh.

--- record.sh
+++ record.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-parec --format=float32le --rate=96000 --channels=1 | ffmpeg -ar 96000 -r 48000 -f f32le -i - output.wav &
+parec --format=float32le --rate=96000 --channels=1 | ffmpeg -ar 96000 -r 48000 -f f32le -i - output.wav
