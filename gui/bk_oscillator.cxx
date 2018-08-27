8a226cb45bf4ea06745e7a2aad6e0dbfb441278f
Ellen Marie Dash <me@duckie.co>
2018-08-27 16:42:14 -0400

    ?????

--- gui/bk_oscillator.cxx
+++ gui/bk_oscillator.cxx
@@ -2,7 +2,7 @@
 #include "bk_oscillator.hxx"
 #include <iostream>
 
-Fl_Round_Button *radio_button_choice(int x, int y, int w, int h,
+Fl_Round_Button *bk_radio_choice(int x, int y, int w, int h,
         const char *label)
 {
     Fl_Round_Button *button = new Fl_Round_Button(x, y, w, h, label);
@@ -10,6 +10,21 @@ Fl_Round_Button *radio_button_choice(int x, int y, int w, int h,
     button->down_box(FL_ROUND_DOWN_BOX);
     return button;
 }
+
+Fl_Slider *bk_slider(int x, int y, int w, int h, const char *label,
+        int minimum, int maximum, int value, int step = 1)
+{
+    Fl_Slider *s = new Fl_Slider(x, y, w, h, label);
+    s->type(1);
+    s->minimum(minimum);
+    s->maximum(maximum);
+    s->step(step);
+    s->value(value);
+    s->align(Fl_Align(FL_ALIGN_LEFT));
+
+    return s;
+}
+
 
 BkOscillator::BkOscillator(int osc_number, const char *name)
 {
@@ -41,37 +56,20 @@ BkOscillator::BkOscillator(int osc_number, const char *name)
     group->labelfont(1);
 
     int radio_top = 40;
-    radio_sawtooth = radio_button_choice(45, radio_top + y_offset,
-                                         radio_width, radio_height,
-                                         "sawtooth");
-    radio_square  = radio_button_choice(140, radio_top + y_offset,
-                                        radio_width, radio_height,
-                                        "square");
+    radio_sawtooth = bk_radio_choice(45, radio_top + y_offset,
+                                     radio_width, radio_height,
+                                     "sawtooth");
+    radio_square  = bk_radio_choice(140, radio_top + y_offset,
+                                    radio_width, radio_height,
+                                    "square");
 
     frequency = new Fl_Spinner(125, 70 + y_offset, 60, 25, "Frequency ");
     frequency->value(30);
 
-    a = new Fl_Slider(60, 110 + y_offset, 256, 25, "A");
-    a->type(1);
-    a->minimum(1);
-    a->maximum(256);
-    a->step(1);
-    a->value(10);
-    a->align(Fl_Align(FL_ALIGN_LEFT));
-
-    b = new Fl_Slider(60, 150 + y_offset, 256, 25, "B");
-    b->type(1);
-    b->minimum(1);
-    b->maximum(256);
-    b->step(1);
-    b->value(10);
-    b->align(Fl_Align(FL_ALIGN_LEFT));
-
-    c = new Fl_Slider(60, 190 + y_offset, 256, 25, "C");
-    c->type(1);
-    c->minimum(1);
-    c->maximum(256);
-    c->step(1);
-    c->value(10);
-    c->align(Fl_Align(FL_ALIGN_LEFT));
+    a = bk_slider(60, 110 + y_offset, 256, 25, "A",
+            1 /* min */, 256 /* max */, 10 /* value */);
+    b = bk_slider(60, 150 + y_offset, 256, 25, "B",
+            1 /* min */, 256 /* max */, 10 /* value */);
+    c = bk_slider(60, 190 + y_offset, 256, 25, "C",
+            1 /* min */, 256 /* max */, 10 /* value */);
 }
