2ca31f5d25d80363d44ca0575b4b9c6ad021a2f7
Ellen Marie Dash <me@duckie.co>
2018-08-27 14:50:19 -0400

    wooooooooo it compiles again

--- gui/barkinator.cxx
+++ gui/barkinator.cxx
@@ -12,35 +12,40 @@ class BkOscillator {
 
         Fl_Group *group;
 
-        Fl_Round_Button *type_sawtooth;
-        Fl_Round_Button *type_square;
+        Fl_Round_Button *radio_sawtooth;
+        Fl_Round_Button *radio_square;
+
+        Fl_Spinner* frequency;
 
         Fl_Slider *a;
         Fl_Slider *b;
         Fl_Slider *c;
+
+        BkOscillator(int osc_number);
 };
 
-BkOscillator::BkOscillator(size_t osc_number) {
+BkOscillator::BkOscillator(int osc_number)
+{
     if (osc_number < 1) {
         std::cerr << "BkOscillator() got an osc_number < 1." << std::endl;
         exit(1);
     }
 
-    size_t x_offset = (25 * osc_number) + (300 * (osc_number - 1));
+    int x_offset = (25 * osc_number) + (300 * (osc_number - 1));
 
     type    = BkSawtooth;
     group   = new Fl_Group(x_offset, 25, 300, 200,
-                           "Oscillator " + std::to_string(osc_number));
+                           "Oscillator N?");
     group->box(FL_THIN_UP_BOX);
     group->labelfont(1);
 
-    sawtooth = new Fl_Round_Button(45, 40, 25, 25, "sawtooth");
-    sawtooth->type(102);
-    sawtooth->down_box(FL_ROUND_DOWN_BOX);
+    radio_sawtooth = new Fl_Round_Button(45, 40, 25, 25, "sawtooth");
+    radio_sawtooth->type(102);
+    radio_sawtooth->down_box(FL_ROUND_DOWN_BOX);
 
-    square = new Fl_Round_Button(140, 40, 25, 25, "square");
-    square->type(102);
-    square->down_box(FL_ROUND_DOWN_BOX);
+    radio_square = new Fl_Round_Button(140, 40, 25, 25, "square");
+    radio_square->type(102);
+    radio_square->down_box(FL_ROUND_DOWN_BOX);
 
     frequency = new Fl_Spinner(125, 70, 60, 25, "Frequency ");
     frequency->value(30);
@@ -70,14 +75,27 @@ BkOscillator::BkOscillator(size_t osc_number) {
     c->align(Fl_Align(FL_ALIGN_LEFT));
 }
 
-Fl_Double_Window* make_window() {
+Fl_Double_Window* make_window()
+{
     Fl_Double_Window* w = new Fl_Double_Window(350, 600, "Window");
     BkOscillator *osc1 = new BkOscillator(1);
     BkOscillator *osc2 = new BkOscillator(2);
+
+    (void)osc1;
+    (void)osc2;
 
     w->end();
     //w->size_range(300, 300, 1000, 1000);
     //w->end();
 
     return w;
+}
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+    make_window();
+
+    return 0;
 }
